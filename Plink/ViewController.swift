2f503dec206fd6f4ed0277caef08406e6b5ffff9
kimiaki <kimioman704@gamil.com>
2019-05-14 20:12:35 +0900

    initial commit

---
+++ Plink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plink
+//
+//  Created by 鈴木公章 on 2019/05/14.
+//  Copyright © 2019年 鈴木公章. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
